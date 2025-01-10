f11de31ee8933c28cc62b6c400fdaa79a4295338
joncaudill <jon.caudill@outlook.com>
2025-01-10 15:39:42 -0500

    everything working except channel link

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/xml"
 	"fmt"
+	"html"
 	"internal/config"
+	"io"
+	"net/http"
 	"os"
 	"time"
 
@@ -30,6 +34,22 @@ type commands struct {
 	names map[string]func(*state, command) error
 }
 
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
 func (c *commands) register(name string, f func(*state, command) error) {
 	//registers a new handler function for a command name
 	c.names[name] = f
@@ -42,6 +62,50 @@ func (c *commands) run(s *state, cmd command) error {
 		return fmt.Errorf("could not run command: %w", err)
 	}
 	return nil
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	//fetches a given RSS feed from a URL
+	request, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("could not create request: %w", err)
+	}
+
+	//set the request headers
+	request.Header.Set("User-Agent", "gator-cli")
+
+	//use client with the context
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, fmt.Errorf("could not fetch feed: %w", err)
+	}
+	defer response.Body.Close()
+
+	//parse the response body
+	var feed RSSFeed
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("could not read response body: %w", err)
+	}
+	fmt.Printf("raw feed: %s\n", body) // Add this line to print the raw XML
+	err = xml.Unmarshal(body, &feed)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse feed: %w", err)
+	}
+	fmt.Printf("unmarshalled feed: %+v\n", feed)          // Add this line to print the unmarshalled struct
+	fmt.Printf("link in struct: %s\n", feed.Channel.Link) // Add this line to print the link field
+	//fmt.Printf("raw feed: %s\n", body)
+	//unescape the HTML entities in the feed
+	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
+	//feed.Channel.Link = html.UnescapeString(feed.Channel.Link) // Add this line to unescape the link
+	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
+	for i := range feed.Channel.Item {
+		feed.Channel.Item[i].Title = html.UnescapeString(feed.Channel.Item[i].Title)
+		feed.Channel.Item[i].Description = html.UnescapeString(feed.Channel.Item[i].Description)
+	}
+
+	return &feed, nil
 }
 
 func handlerLogin(s *state, cmd command) error {
@@ -118,6 +182,37 @@ func handlerList(s *state, cmd command) error {
 	return nil
 }
 
+func handlerAgg(s *state, cmd command) error {
+	//func that aggregates the RSS feeds
+
+	//the fake feed URL is used for testing
+	//remove in production
+	cmd.args = []string{"https://www.wagslane.dev/index.xml"}
+
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("agg command requires 1 or more arguments")
+	}
+
+	for _, feedURL := range cmd.args {
+		feed, err := fetchFeed(context.Background(), feedURL)
+		if err != nil {
+			fmt.Printf("could not fetch feed: %s", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Feed: %s\n", feed.Channel.Title)
+		fmt.Printf("Link: %s\n", feed.Channel.Link)
+		fmt.Printf("Description: %s\n", feed.Channel.Description)
+		fmt.Println("Items:")
+		for _, item := range feed.Channel.Item {
+			fmt.Printf("  * %s\n", item.Title)
+			fmt.Printf("    %s\n", item.Link)
+			fmt.Printf("    %s\n", item.Description)
+			fmt.Printf("    %s\n", item.PubDate)
+		}
+	}
+	return nil
+}
+
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
@@ -138,6 +233,7 @@ func main() {
 	cliCommands.register("register", handlerRegister)
 	cliCommands.register("reset", handlerReset)
 	cliCommands.register("users", handlerList)
+	cliCommands.register("agg", handlerAgg)
 
 	args := os.Args
 	if len(args) < 2 {
